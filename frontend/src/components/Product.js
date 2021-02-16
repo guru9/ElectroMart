b5dc802fb55cf50ebef3ce6ff2b228456c3d9543
BHARATM-W10\Bharat Moger <gururaj9m@gmail.com>
2021-02-16 18:26:21 +0530

    initial setup to backend

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,7 +4,7 @@ import Ratings from './Ratings'
 const Product = ({ product }) => {
   return (
     <Card className='my-4 p-3'>
-      <a href={`/product/${product.id}`} className='prod-thumb'>
+      <a href={`/product/${product._id}`} className='prod-thumb'>
         <Card.Img variant='top' className='rounded' src={product.image} />
         <Card.Title className='pt-1 prod-name'>{product.name}</Card.Title>
       </a>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,66 @@
-const ProductScreen = () => {
-  return <div>product screen</div>
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Ratings from '../components/Ratings'
+import products from '../mockApi/products'
+
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id)
+  const productStatus = product.countInStock === 0 ? 'Out of Stock' : 'In Stock'
+  return (
+    <>
+      <Link className='btn btn-dark my-4' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h4>{product.name}</h4>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Ratings
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price: <strong>${product.price}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <ListGroup>
+            <ListGroup.Item>
+              <Row>
+                <Col>Price</Col>
+                <Col>${product.price}</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Status</Col>
+                <Col>{productStatus}</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Link
+                to='/cart'
+                className={`btn btn-dark btn-block ${
+                  product.countInStock === 0 ? 'disabled' : ''
+                }`}
+              >
+                Add To Cart
+              </Link>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default ProductScreen
