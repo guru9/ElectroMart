be6f58f619d42b2f91413984f6347db00b319418
BHARATM-W10\Bharat Moger <gururaj9m@gmail.com>
2021-02-18 17:27:17 +0530

    added dotenv for processing sensitive env information.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./mockApi/products')
+
+dotenv.config()
+
 const app = express()
 
 app.get('/', (req, res) => {
@@ -15,4 +19,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  5000,
+  console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+)
