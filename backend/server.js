9546b49d870f220ea243817c5f017f4e9be73c37
BHARATM-W10\Bharat Moger <gururaj9m@gmail.com>
2021-02-20 18:01:08 +0530

    mongodb props added and connected to database.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './mockApi/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
