ee93df61951cfc29412c7a0e95ab9af676af3fbf
BHARATM-W10\Bharat Moger <gururaj9m@gmail.com>
2021-03-02 13:09:06 +0530

    user authentication has been added.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -9,11 +10,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('api is running..')
 })
 
 app.use('/api/products/', productRoutes)
+app.use('/api/users/', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
