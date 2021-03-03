6a39893e15873e7bece3badaa4f7c6cc3491cf98
BHARATM-W10\Bharat Moger <gururaj9m@gmail.com>
2021-03-03 18:20:11 +0530

    user profile server has been added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,7 +25,7 @@ const authUser = asyncHandler(async (req, res) => {
 
 // @desc   get user profile
 // @route  GET /api/users/profile
-// @access Public
+// @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
@@ -75,4 +75,32 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile, registerUser }
+// @desc   update user profile
+// @route  PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('User not found.')
+  }
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile }
